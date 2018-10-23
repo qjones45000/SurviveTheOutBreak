f2c9eca2b476fcaedb0183d1ae339533a9e8ba31
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2018-10-22 23:20:58 -0400

    alien stuff

---
+++ alien dude/Assets/animations.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animations : MonoBehaviour {
+
+   private Animator _anim;
+    public float MaxSpeed = 50f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _anim = GetComponent<Animator>();	
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        var x = Input.GetAxis("Vertical");
+        var y = Input.GetAxis("Horizontal");
+       
+
+        Move(x, y);
+
+      
+
+	}
+
+    public void Move(float x, float y)
+    {
+        _anim.SetFloat("Vy", y);
+        _anim.SetFloat("Vx" , x);
+
+        transform.position += Vector3.forward *MaxSpeed * x * Time.deltaTime;
+        transform.position += Vector3.right * MaxSpeed * y * Time.deltaTime;
+    }
+
+}
---
+++ alien dude/Assets/thirdpersoncam.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdpersoncam : MonoBehaviour {
+
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+    public Transform LookAt;
+    public Transform camTransform;
+
+    private Camera cam;
+
+    private float distance = 10.0f;
+    private float Currentx = 0.0f;
+    private float Currenty = 0.0f;
+  
+
+	// Use this for initialization
+	void Start () {
+
+        camTransform = transform;
+        cam = Camera.main;
+
+		
+	}
+
+    private void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(Currenty, Currentx, 0);
+        camTransform.position = LookAt.position + rotation * dir;
+        camTransform.LookAt(LookAt.position);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        Currentx += Input.GetAxis("Mouse X");
+        Currenty += Input.GetAxis("Mouse Y");
+
+        Currenty = Mathf.Clamp(Currenty,Y_ANGLE_MIN, Y_ANGLE_MAX);
+	}
+}

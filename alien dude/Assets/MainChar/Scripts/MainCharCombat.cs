cbac27d7ef45ea139b5566c58a729da156750b5a
qjones45000 <36212647+qjones45000@users.noreply.github.com>
2019-04-10 17:09:06 -0400

    heavy combo complete

--- alien dude/Assets/MainChar/Scripts/MainCharCombat.cs
+++ alien dude/Assets/MainChar/Scripts/MainCharCombat.cs
@@ -6,11 +6,15 @@ public class MainCharCombat : MonoBehaviour {
 
     private Animator anim;
 
+    public float measureCombo;
+
     private AnimatorStateInfo CombatState;
+    private AnimatorStateInfo HeavyState;
 
-    static int Slash1 = Animator.StringToHash("Combat. Swing1");
+    static int Slash1 = Animator.StringToHash("Combat.isSwinging1");
+    static int slash2 = Animator.StringToHash("Combat.isSwinging2");
     static int Fight = Animator.StringToHash("Combat.isFighting");
-
+    static int heavy1 = Animator.StringToHash("HeavyAttack.Heavy1");
 	// Use this for initialization
 	void Start ()
     {
@@ -21,10 +25,12 @@ public class MainCharCombat : MonoBehaviour {
 	void Update ()
     {
         CombatState = anim.GetCurrentAnimatorStateInfo(1);
-
+        HeavyState = anim.GetCurrentAnimatorStateInfo(2);
      
 
         charCombat();
+
+        BigCombos();
 	}
 
     void charCombat()
@@ -45,6 +51,62 @@ public class MainCharCombat : MonoBehaviour {
             anim.SetBool("Combo1", false);
         }
 
+        if (CombatState.nameHash == Slash1 && Input.GetButtonDown("R1"))
+        {
+            anim.SetBool("Combo2", true);
+            anim.SetBool("Combo1", false);
+
+            Debug.Log("In slash state");
+        }
+        else
+        {
+            anim.SetBool("Combo2", false);
+        }
+        if(CombatState.nameHash == slash2 && Input.GetButtonDown("R1"))
+        {
+            anim.SetBool("Combo3", true);
+            anim.SetBool("Combo2", false);
+        }
+        else
+        {
+            anim.SetBool("Combo3", false);
+        }
+
+
         
+    }
+
+    void BigCombos()
+    {
+        if (Input.GetButton("R1"))
+        {
+            measureCombo += 0.1f;
+            Debug.Log(measureCombo);
+          
+        }
+        else if (Input.GetButtonUp("R1"))
+        {
+            measureCombo = 0;
+            Debug.Log(measureCombo);
+        }
+
+        if(CombatState.nameHash == Fight && measureCombo >= 1)
+        {
+            anim.SetBool("LargeCombo1",true);
+        }
+        else
+        {
+            anim.SetBool("LargeCombo1", false);
+        }
+
+        if( measureCombo > 4.50)
+        {
+            anim.SetBool("LargeCombo2", true);
+        }
+        else
+        {
+            anim.SetBool("LargeCombo2", false);
+        }
+
     }
 }
